4e02dc7aea930b05440b9b9bd2547f6330877566
abnik519 <aritra.banik519@gmail.com>
2021-01-09 23:35:53 -0500

    Edited about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import topImage from '../assets/constructed/Cell_tower_graphic.png';
 export class about extends Component {
   render() {
     return (
-        <div id="about">
-        <div className="container">
-          <div className="row">
-            
+      <div id="about">
+        <div className="aboutContainer">
+          <div className="topAboutBack">
+            <img src={topImage} style={{width: "50%", marginTop: "-3vw"}}></img>
+            <span className="mainSubtitle">Canada's <span style={{color: "#ff0000"}}>First</span></span>
+            <span className="smallSubtitle"><span style={{color: "#ff0000"}}>Tech</span> Case Competition</span>
           </div>
+          <p className="aboutTitle">What is Net<span style={{color: "#ff0000"}}>X</span></p>
+          <p className="aboutText">incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea
+            Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea
+          </p>
         </div>
       </div>
     )
--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,16 @@ import mainPage from '../assets/constructed/Landing_page_graphic.png';
 export class Header extends Component {
   render() {
     return (
-      <div className="topLandingBack">
-        <img src={logo} className="landLogo"></img>
-        <img src={mainPage} className="landGraphic"></img>
-        <p className="landText">Case Competition and Network Event</p>
-        <p className="landSubtitle">March 5-6th 2021 <br></br> Virtual event</p>
-        <button className="applyButton">Apply Now</button>
+      <div id="top-page">
+        <div className="landContainer">
+          <div className="topLandingBack">
+            <img src={logo} className="landLogo"></img>
+            <div><img src={mainPage}  className="landGraphic"></img></div>
+            <p className="landText">Case Competition and Network Event</p>
+            <p className="landSubtitle">March 5-6th 2021 {"\n"} <p style={{fontSize: "2vw", marginTop: "-.3vw", marginLeft: ".5vw"}}>Virtual event</p> </p>
+            <button className="applyButton">Apply Now</button>
+          </div>
+        </div>
       </div>
     );
   }
