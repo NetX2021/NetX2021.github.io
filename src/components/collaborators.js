9730c15921787c3e3ac2139234d20a20eb0f1772
Hector Garrigues <hectorgarrigues@Hectors-MacBook-Air.local>
2021-01-12 01:05:01 -0500

    finished first version of collabs

--- src/components/collaborators.js
+++ src/components/collaborators.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import motherboard from '../assets/constructed/Circuit_board.png';
+import mcgillBuildings from '../assets/constructed/McGill_Buildings.png';
 
 export class Collaborators extends Component {
     
@@ -7,14 +8,27 @@ export class Collaborators extends Component {
     render() {
         return (
             <div id="collaborators">
-
+                <div className="collabBuildings">
+                    <img src={mcgillBuildings} className="buildingPicItself" alt="main mcgill buildings" />
+                </div>
                 <div className="collabDarkBox">
                     <span className="topCollab">Our <span style={{color: "#ff0000"}}>Partners</span></span>
                     <div className="twoCollabLogos">
                         <div className="tempCircle"></div>
                         <div className="tempCircle"></div>
                     </div>
+                    <span className="invisibleCollab">Keynote <span style={{color: "#ffffff"}}>Speakers</span></span>
                     <img src={motherboard} className="motherBoard" alt="motherboard design"/>
+                </div>
+                <div className="collabLightBox">
+                    <div className="tempCircleDark"></div>
+                    <div className="tempCircleDark"></div>
+                    <div className="tempCircleDark"></div>
+                </div>
+                <div className="collabLightBox">
+                    <div className="collabNames"></div>
+                    <div className="collabNames"></div>
+                    <div className="collabNames"></div>
                 </div>
             </div>
         );
