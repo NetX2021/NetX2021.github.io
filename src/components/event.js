25541022f3a5e8e658b3c562ddf31c204d8e65c9
abnik519 <aritra.banik519@gmail.com>
2021-01-19 17:42:30 -0500

    Header responsive

--- src/components/event.js
+++ src/components/event.js
@@ -7,36 +7,30 @@ export class event extends Component {
         <div id="event">
           <div className="eventContainer">
             <div className="topEventTitle">
-              More Than a <span style={{marginLeft: "2vw", color: "#ff0000"}}> Conference</span>
+              More Than a <span style={{marginLeft: "20px", color: "#ff0000"}}> Conference</span>
             </div>
 
             {/* Network Event */}
             <div className="eventBox">
               <div className="eventTwoRows">
-                <div className="eventBoxTitle"><span style={{color: "#ff0000"}}>Network</span> Event...</div>
+                <div className="eventTwoColumns">
+                  <div className="eventBoxTitle"><span className="eventBoxSpan">Network</span> Event</div>
+                  <div className="eventBoxTitle"><span className="eventBoxSpan">Case</span> Comp</div>
+                </div>
+
+                {/* Network Event */}
                 <div className="eventTwoColumns">
                   <div className="eventImgColumn">
-                    <img src={phoneImage} style={{width: "100%"}} alt="Phone with various icons on top of it"></img>
+                    <img src={phoneImage} className="eventBoxImg" alt="Phone with various icons on top of it"></img>
                   </div>
                   <div className="eventTextColumn">
-                    <div className="eventBoxText"> Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum
-                    dolore eu fugiat nulla pariatur.
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse
+                    <div className="eventBoxText"> 
+                    As a networking event, NetX will enable students to meet representatives of different tech startup companies as well as 
+                    speakers of various backgrounds relevant to the industry. Students will then get a chance to hear about current projects 
+                    and research all while discovering new technology currently being applied in the field. 
+
                     </div>
                   </div>
-                  <i class="arrow right"></i>
                 </div>
               </div>
             </div>
