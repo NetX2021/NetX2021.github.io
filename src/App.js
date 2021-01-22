b79a9ad691c002a5864b28768d1a951eddac4f7e
abnik519 <aritra.banik519@gmail.com>
2021-01-22 16:39:19 -0500

    Improved Media Queries

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export class App extends Component {
 
   render() {
     return (
-      <div>
+      <div style={{overflow: "hidden"}}>
         <img src={road} className="bigRoad" alt="road that goes through whole page"></img>
         <Navigation />
         <Header data={this.state.landingPageData.Header} />
