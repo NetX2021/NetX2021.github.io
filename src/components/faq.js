d2a6dee392f9e9f6c858d9068ff1714dac3417dd
Ade <adeend1234@gmail.com>
2021-08-10 15:23:32 -0400

    Add slide to Hear from our participants (previously faq) page

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,120 +1,126 @@
 import React, { Component } from 'react'
 import faqImage from "../assets/constructed/Faq_graphic.png";
 import dropdown from "../assets/dropdown.png"
+import hopper from "../assets/Hopper.png";
+import haply from "../assets/Haply.png";
+import notos from "../assets/Notos Technologies.png";
+import data from "../assets/Dataperformers.png";
+import { Slide } from "react-slideshow-image";
 
 export class faq extends Component {
-  state = { showingone: true };
-  state = { showingtwo: true };
-  state = { showingthree: true };
-  state = { showingfour: false };
-  
+  state = {
+    index: 0,
+    direction: null,
+  };
+
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction,
+    });
+  }
+
   render() {
-    const { showingone } = this.state;
-    const { showingtwo } = this.state;
-    const { showingthree } = this.state;
-    const { showingfour } = this.state;
+
+    const properties = {
+      controls: true,
+      indicators: true,
+      duration: 7000,
+      infinite: true,
+    };
+
     return (
       <div id="faq">
+      <div className="faqTitle">Hear from our <span style={{color:"#ff0000"}}>Participants</span></div>
         <div className="faqContainer">
             <div className="faqImgColumn">
                 <img src={faqImage} className="faqImage" alt="FAQ city scape"></img>
             </div>
-            <div className="faqTextColumn">
-                <div className="faqTitle">FAQ'S</div>
-                { !showingone 
-                    ? <button className="faqButton" onClick={() => this.setState({ showingone: !showingone })}>
-                      Can we apply in teams?
-                      <img className="rightButtonArrow"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingone 
-                    ? <button className="faqButton faqclicked" onClick={() => this.setState({ showingone: !showingone })}>
-                      Can we apply in teams?
-                      <img className="rightButtonArrow clicked"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingone
-                  ? <div className="faqInfo">Applicants can submit names of other applicants they wish to team up with on their registration form and will be placed together if both 
-                    applicants are selected. Otherwise, applicants will be placed in teams by our selection committee.
-                  </div>
-                  : null
-                }
-
-                { !showingtwo 
-                    ? <button className="faqButton" onClick={() => this.setState({ showingtwo: !showingtwo })}>
-                      How many people can I have on my team?
-                      <img className="rightButtonArrow"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingtwo 
-                    ? <button className="faqButton faqclicked" onClick={() => this.setState({ showingtwo: !showingtwo })}>
-                      How many people can I have on my team?
-                      <img className="rightButtonArrow clicked"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingtwo
-                  ? <div className="faqInfo">Teams will consist of 2 participants</div>
-                  : null
-                }
-
-                { !showingthree 
-                    ? <button className="faqButton" onClick={() => this.setState({ showingthree: !showingthree })}>
-                      When does the event happen?
-                      <img className="rightButtonArrow"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingthree 
-                    ? <button className="faqButton faqclicked" onClick={() => this.setState({ showingthree: !showingthree })}>
-                      When does the event happen?
-                      <img className="rightButtonArrow clicked"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingthree
-                  ? <div className="faqInfo">The event will be held on the weekend of March 20th</div>
-                  : null
-                }
-
-                { !showingfour
-                    ? <button className="faqButton" onClick={() => this.setState({ showingfour: !showingfour })}>
-                      How much does it cost to attend?
-                      <img className="rightButtonArrow"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingfour 
-                    ? <button className="faqButton faqclicked" onClick={() => this.setState({ showingfour: !showingfour })}>
-                      How much does it cost to attend?
-                      <img className="rightButtonArrow clicked"
-                      src={dropdown}
-                      alt="dropdown"/>
-                    </button>
-                    : null
-                }
-                { showingfour
-                  ? <div className="faqInfo">The event is free for all applicants!</div>
-                  : null
-                }
+          <div className="faqTextColumn">
+          <div className="participantBox">
+          <Slide {...properties}>
+            <div className="each-slide">
+              <div className="participantSlide">
+                <a
+                  href="https://www.hopper.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={hopper} className="hopperLogo" alt="hopper logo" />
+                </a>
+                <p className="hopperText">
+                Founded in Montreal, Hopper is a prediction algorithm app enabling customers to save 
+                money on  flight and hotel prices by booking at the right time! With innovative AI 
+                and data-driven technology, Hopper aims to build a transparent travel marketplace 
+                seeking to reduce traveler anxiety and help clients plan their next adventure.
+                </p>
+              </div>
             </div>
+            <div className="each-slide">
+              <div className="participantSlide">
+                <div className="participantSlide">
+                  <a
+                    href="https://www.notostechnologies.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={notos}
+                      className="notosLogo"
+                      alt="notos technologies logo"
+                    />
+                  </a>
+                  <p className="notosText">
+                  Notos Technologies is a software company providing better solutions for UAV flight optimization. 
+                  Through AI and aerial data, Notos’  software allows UAVs to autonomously find and exploit wind 
+                  energies in the atmosphere, increasing flight efficiency, safety and range.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div className="participantSlide">
+                <a
+                  href="https://www.dataperformers.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={data}
+                    className="dataLogo"
+                    alt="dataperformers logo"
+                  />
+                </a>
+                <p className="dataText">
+                    Dataperformers is an Applied AI company from Montreal building advanced and cross-industry technologies and products.
+                     More than a simple algorithm, Dataperformers aims to utilize AI and big data to provide end-to-end solutions for complex manufacturing, 
+                     aerospace and supply chain needs.
+                </p>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div className="participantSlide">
+                <a
+                  href="https://haply.co"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={haply}
+                    className="haplyLogo"
+                    alt="haply logo"
+                  />
+                </a>
+                <p className="dataText">
+                  Haply Robotics specializes in the sense of touch and are creating a physical simulation hardware for surgical simulation and tactical training. 
+                  Their work is using cutting edge technologies in robotics, virtual reality, simulation design, and 6 degree-of-freedom motion tracking to create 
+                  a platform that will shape the way we train, learn, and entertain ourselves far into the future.
+                </p>
+              </div>
+            </div>
+          </Slide>
+        </div>
+        </div>
           </div>
       </div>
     )
